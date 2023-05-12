75ebf98511b182f974892676034a34858a9637ff
youbaAg <92794315+youbaAg@users.noreply.github.com>
2023-05-12 21:07:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,30 +4,37 @@ import joblib
 
 app = Flask(__name__)
 
-
-
 # Charger le modèle entraîné à partir du fichier
 model = joblib.load('model.joblib')
 
 # Charger les colonnes à partir du fichier
 with open('columns.txt', 'r') as f:
     columns = f.read().splitlines()
-    
+
 # Endpoint pour faire des prédictions
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    # Récupérer les données de test
-    test_data = request.json
-    
+    if request.method == 'POST':
+        # Récupérer les données de test à partir du corps de la demande JSON
+        test_data = request.json
+    else:
+        # Récupérer les données de test à partir des paramètres de l'URL
+        test_data = {
+            'price': request.args.get('price'),
+            'color': request.args.get('color'),
+            'brand': request.args.get('brand'),
+            'category': request.args.get('category')
+        }
+
     # Transformer les données en DataFrame
     test_df = pd.DataFrame(test_data, index=[0])
-    
+
     # Encoder les données en utilisant les colonnes d'encodage one-hot
     test_df = pd.get_dummies(test_df)
     test_df = test_df.reindex(columns=columns, fill_value=0)
-    
+
     # Faire la prédiction
     prediction = model.predict(test_df)
-    
+
     # Retourner la prédiction en format JSON
     return jsonify({'prediction': prediction[0]})
